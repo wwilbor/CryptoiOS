ecd67365031060e401e3948f9bc95a3fc331cc51
wwilbor <wilson.wistuba@gmail.com>
2016-06-17 18:52:52 -0300

    first commit

---
+++ Crypto/NSData+AES256.m
@@ -0,0 +1,9 @@
+//
+//  NSData+AES256.m
+//  Crypto
+//
+//  Created by Wilson Wistuba on 6/17/16.
+//  Copyright © 2016 Wilson Wistuba. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
