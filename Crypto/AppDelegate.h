f34751dede182f55bf43981debf464840efcf102
wwilbor <wilson.wistuba@gmail.com>
2016-06-17 11:57:57 -0300

    Initial Commit

---
+++ Crypto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Crypto
+//
+//  Created by Wilson Wistuba on 6/17/16.
+//  Copyright © 2016 Wilson Wistuba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Crypto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Crypto
+//
+//  Created by Wilson Wistuba on 6/17/16.
+//  Copyright © 2016 Wilson Wistuba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
